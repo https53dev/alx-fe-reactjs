8772be0ba905b32457a6d8e9765784c8b0461048
https53dev <adeigbeyussuf5@gmail.com>
2025-03-16 13:05:20 +0000

    tailwind

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  purge: [
+    './src//*.html',
+    './src//*.js',
+    './src//*.jsx',
+    './src//*.ts',
+    './src//*.tsx',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

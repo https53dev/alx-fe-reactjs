5fe4fa85d13d130f8da0e33f792ae323812a694d
https53dev <adeigbeyussuf5@gmail.com>
2025-03-20 14:16:31 +0000

    recipe-sharing-platform

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3490dc',
+        secondary: '#ffed4a',
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
